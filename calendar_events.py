44101ed67a899587f8fc44c93e576ef7e8f1683d
MichaelJS <mowz2012@gmail.com>
2025-07-14 12:04:29 -0700

    Add files via upload

---
+++ calendar_events.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+class Event:
+    def __init__(self, title, date, description, is_done):
+        self.title = title
+        self.date = date
+        self.description = description
+        is_done = is_done
+
+    def to_dict():
+        pass
+
+    def from_dict():
+        pass
+
+    def format():
+        pass
+
+class Calendar:
+    pass
