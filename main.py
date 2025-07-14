97d2d916f60f283246911ebfcf2e4582eb1cc40b
MichaelJS <mowz2012@gmail.com>
2025-07-14 12:05:33 -0700

    Updated: main.py
    I added some new features and shifted things around while bug testing the habit tracker.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from task_manager import Task, TaskManager
+from task_manager import TaskManager
 from colorama import init, Fore
 from difflib import get_close_matches
 import user
-from habit_tracker import Habit, HabitTracker
+from habit_tracker import HabitTracker
 
 init(autoreset=True)
 
@@ -10,10 +10,10 @@ def match_input(user_input, options, cutoff=0.6):
     return get_close_matches(user_input.lower(), options, n=1, cutoff=cutoff)
 
 if __name__ == '__main__':
-    # The sign-in options:
     while True:
         manager = TaskManager(tasks=[], user_name='')
         habit = HabitTracker(habits=[])
+
         login_data = input(
             "Welcome to my task manager app, to start please type in one of the following numbers:\n"
             "\n"
@@ -26,29 +26,35 @@ if __name__ == '__main__':
 
         if refined_login and refined_login[0] == '1':
             user_data = user.load_user()
-            if user_data is not None:
+            if user_data and hasattr(user_data, 'user_name') and hasattr(user_data, 'tasks'):
                 manager.user_name = user_data.user_name
                 manager.tasks = user_data.tasks
             else:
-                print(Fore.RED + "Failed to load user data.")
+                print(Fore.RED + "Failed to load user data. Please try again or create a new account.")
                 continue
 
         elif refined_login and refined_login[0] == '2':
             user_data = user.create_new_user()
-            if user_data is not None:
+            if user_data and hasattr(user_data, 'user_name') and hasattr(user_data, 'tasks'):
                 manager.user_name = user_data.user_name
-                manager.tasks = []
+                manager.tasks = user_data.tasks
                 manager.list_tasks()
             else:
-                print(Fore.RED + "Failed to create new user.")
+                print(Fore.RED + "Failed to create new user. Please try again.")
                 continue
 
+        elif refined_login and refined_login[0] == '3':
+            print("Okay, exiting program...")
+            exit()
+
         else:
-            print(Fore.RED + f"Sorry, {login_data} is not a valid option.")
-        # The main menu options:
+            print(Fore.RED + f"Sorry, '{login_data}' is not a valid option.")
+            continue
+
+        # Main menu
         while True:
             user_choice = input("Welcome to my task manager app!\n"
-            "Please choose one of the following to do using it's number:\n"
+            "Please choose one of the following to do using its number:\n"
             "\n"
             "1. Use the habit tracker.\n"
             "2. Add tasks.\n"
@@ -58,58 +64,58 @@ if __name__ == '__main__':
             "6. Sort tasks.\n"
             "7. Save task.\n"
             "8. Load task.\n"
-            "9. Exit\n")
+            "9. Calendar & Events.\n"
+            "10. Exit\n")
+
             refined_user_choice = match_input(user_choice, ['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+
             if refined_user_choice and refined_user_choice[0] == '1':
-                # The habit options:
                 while True:
-                    habit_choice = input("Welcome to the habit manager! Please choose one of the following to do:\n"
-                    "1. View habits.\n" 
-                    "2. Add habit.\n"
-                    "3. Mark habit as complete.\n"
-                    "4. Back")
-                    refined_user_choice = match_input(habit_choice, ['1', '2', '3', '4'])
-                    if refined_user_choice and refined_user_choice[0] == '1':
+                    habit_choice = input("Welcome to the habit manager! Please choose one of the following:\n"
+                        "1. View habits.\n" 
+                        "2. Add habit.\n"
+                        "3. Mark habit as complete.\n"
+                        "4. Save habit.\n"
+                        "5. Load habit.\n"
+                        "6. Back\n")
+
+                    refined_habit_choice = match_input(habit_choice, ['1', '2', '3', '4', '5', '6'])
+
+                    if refined_habit_choice and refined_habit_choice[0] == '1':
                         habit.list_habits()
-
-                    elif refined_user_choice and refined_user_choice[0] == '2':
+                    elif refined_habit_choice and refined_habit_choice[0] == '2':
                         habit.add_habit()
-
-                    elif refined_user_choice and refined_user_choice[0] == '3':
+                    elif refined_habit_choice and refined_habit_choice[0] == '3':
                         habit.mark_habit()
-
-                    elif refined_user_choice and refined_user_choice[0] == '4':
+                    elif refined_habit_choice and refined_habit_choice[0] == '4':
+                        habit.save(username=user_data.user_name)
+                    elif refined_habit_choice and refined_habit_choice[0] == '5':
+                        habit.load(username=user_data.user_name)
+                    elif refined_habit_choice and refined_habit_choice[0] == '6':
                         break
-                                            
                     else:
-                        print(Fore.RED + f"Sorry, {habit_choice} is not a valid option.")
+                        print(Fore.RED + f"Sorry, '{habit_choice}' is not a valid option.")
                         continue
 
             elif refined_user_choice and refined_user_choice[0] == '2':
                 manager.add_task()
-
             elif refined_user_choice and refined_user_choice[0] == '3':
                 manager.list_tasks()
-
             elif refined_user_choice and refined_user_choice[0] == '4':
                 manager.remove_task()
-
             elif refined_user_choice and refined_user_choice[0] == '5':
                 manager.mark_task()
-        
             elif refined_user_choice and refined_user_choice[0] == '6':
                 manager.sort_tasks()
-     
             elif refined_user_choice and refined_user_choice[0] == '7':
                 manager.save_to_file()
-        
             elif refined_user_choice and refined_user_choice[0] == '8':
                 manager.load_from_file()
-
             elif refined_user_choice and refined_user_choice[0] == '9':
+                pass # This will be where the calender and events options go.
+            elif refined_user_choice and refined_user_choice[0] == '10':
                 print("Okay, goodbye!")
                 exit()
-                                    
             else:
-                print(Fore.RED + f"Sorry, {user_choice} is not a valid option.")
+                print(Fore.RED + f"Sorry, '{user_choice}' is not a valid option.")
                 continue
